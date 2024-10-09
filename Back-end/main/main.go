89fcb17ff4db224f9a7e97594ddd88f402f4660a
JeronimoNP <familiadojeronimo@gmail.com>
2024-10-09 19:44:02 -0300

    update

--- Back-end/main/main.go
+++ Back-end/main/main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 )
 
+// Função principal de inicialização do http
 func main() {
+	//configurando resposta de escrita ao entrar no servidor, avisando que está funcionando.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
 	})
-
-	fs := http.FileServer(http.Dir("static/"))
+	//local onde vai ficar os arquivos de requisição de pagina principal.
+	fs := http.FileServer(http.Dir("../Estudos/Front-End/main"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	http.ListenAndServe(":80", nil)
